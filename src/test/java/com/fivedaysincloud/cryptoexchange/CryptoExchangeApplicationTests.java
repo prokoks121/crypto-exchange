13ad89a146dd7993d2f4176bbf48f0b4c78cbd1d
mihailoaleksic <mihailo.aleksic@partner.digitalchargingsolutions.com>
2022-11-15 19:12:28 +0100

    Initial project setup

---
+++ src/test/java/com/fivedaysincloud/cryptoexchange/CryptoExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivedaysincloud.cryptoexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
